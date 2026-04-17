817a6c20eb04a57a27ad93addc0af270059c5cd7
MariaBovo <eduardabovo826@gmail.com>
2026-04-17 11:36:43 -0300

    Adicionando mais classes
    Adicionando as classes de pet, users e zoonoses, junto com os seus atributos.

--- system/conac.py
+++ system/conac.py
@@ -1,5 +1,29 @@
 from dataclasses import dataclass
+from date import datetime
 
 @dataclass
 class CONAC:
     pass
+
+@dataclass
+class pet:
+    id: int
+    type: str
+    breed: str
+    sex: bool
+    has_active_zoonosis: bool
+    
+@dataclass
+class user:
+    id: int
+    full_name: str
+    username: str
+    created: datetime
+    privileged: bool
+    
+@dataclass
+class zoonoses:
+    id: int
+    illness: str
+    details: str
+
